97f232d1053f5749b508f2607611df0bc9e59564
André Caldeira <59808571+andrecaldeiras@users.noreply.github.com>
2023-04-13 16:29:46 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,11 @@
 const submit = document.querySelector(".card__button");
-const inputNumber = document.querySelectorAll(".card__input");
 const cardSubmitted = document.querySelector(".card__state--submitted");
 const cardNormal = document.querySelector(".card__state");
 const selectedText = document.querySelector(".submitted__illustration--text");
 
-let clicked = false;
-let rating = 0
-
-for (const value of inputNumber) {
-    value.addEventListener("click", function() {
-        cleanValues();
-        value.classList.toggle("card__input--selected");
-        rating = value.textContent;
-    });
-}
-
-function cleanValues() {
-    for (const value of inputNumber) {
-        value.classList.remove("card__input--selected");
-    }
-}
-
 submit.addEventListener("click", function() {
+    const rating = document.querySelector(".ratings-input:checked").value;
+    console.log(rating)
     if (rating == 0) {
         alert("Please input a valid rating");
     }
